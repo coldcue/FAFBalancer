18fec01d3301cd945c84c48dd790a2861b5d560a
Andras Szell <szella@sonrisa.hu>
2015-11-23 19:36:39 +0100

    Seems to work

--- src/com/andrewszell/fafbalancer/Main.java
+++ src/com/andrewszell/fafbalancer/Main.java
@@ -2,6 +2,7 @@ package com.andrewszell.fafbalancer;
 
 import com.andrewszell.fafbalancer.maps.GameMap;
 import com.andrewszell.fafbalancer.maps.GapOfRohan5V5GameMap;
+import com.andrewszell.fafbalancer.maps.SentonsClutchGameMap;
 import com.andrewszell.fafbalancer.maps.slot.SlotAssignment;
 import com.andrewszell.fafbalancer.maps.slot.SlotMovement;
 import com.andrewszell.fafbalancer.partition.Partitioner;
@@ -12,12 +13,26 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        GameMap map = new GapOfRohan5V5GameMap();
-
+    public static void main(String[] args) throws Exception {
+        GameMap map;
         List<Player> players = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please select map: [0:Senton's Clutch, 1:GapOfRohan5V5]");
+        System.out.print("> ");
+        switch (scanner.nextInt()) {
+            case 0:
+                map = new SentonsClutchGameMap();
+                break;
+            case 1:
+                map = new GapOfRohan5V5GameMap();
+                break;
+            default:
+                throw new Exception("Unknown map");
+        }
+
+
         System.out.println("Please type in the ratings: ");
         for (int i = 0; i < map.getTeamCount() * map.getTeamPlayerCount(); i++) {
             System.out.print("> ");
